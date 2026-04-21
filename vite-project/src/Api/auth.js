5f7288cd185ab5ed61f27d161cb2e461d87cd496
Yang <ycj3294@gmail.com>
2026-04-21 14:48:40 +0900

    로그인 연결

---
+++ vite-project/src/Api/auth.js
@@ -0,0 +1,22 @@
+// src/api/auth.js
+import client from "./client";
+
+// 회원가입
+export const signup = async (data) => {
+  const res = await client.post("auth/signup", data);
+  return res.data;
+};
+
+// 로그인
+export const login = async (data) => {
+  const res = await client.post("auth/login", data);
+  return res.data;
+};
+
+// 토큰 재발급
+export const refreshToken = async (refresh_token) => {
+  const res = await client.post("auth/refresh", {
+    refresh_token,
+  });
+  return res.data;
+};
